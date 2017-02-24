42f840f353a66ec4a37b3d49adce12504274e63d
tetralli <Scuflaire.Thomas@live.be>
2017-02-24 11:33:41 +0100

    Update index.php

--- web/b2b/index.php
+++ web/b2b/index.php
@@ -1,1 +1,13 @@
-<? echo 'Bienvenue sur le site b2b.wt17.ephec-ti.be' ?>
+<?php echo '
+<html>
+  <body style="text-align:center;">
+    <h1>Bienvenue sur le site b2b.wt17.ephec-ti.be</h1>
+    <p> groupe 17 <p>
+    <p> Scuflaire Thomas<p>
+    <p> Marquebreucq Emmanuel</p>
+    <p> Brancart Clément </p>
+    <hr>
+    <p> voici un tableau provenant de la db </p>'
+  </body>
+</html>
+?>
