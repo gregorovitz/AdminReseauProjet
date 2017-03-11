f4a80c21b121deb0a0fe8d7eff70a87c7d983025
tetralli <Scuflaire.Thomas@live.be>
2017-03-11 02:13:51 +0100

    updates

---
+++ web-SSL(Expérimental)/b2b/index.php
@@ -0,0 +1,43 @@
+<html>
+	<head>
+		<link rel=stylesheet href="style.css">
+		<title>Site Dynamique</title>
+		<meta charset="UTF-8">
+	</head>
+  <body>
+	<nav>
+		<ul>
+		 <li><a href=http://www.wt17.ephec-ti.be>Site statique</a></li>
+		 <li><a href=http://b2b.wt17.ephec-ti.be>Site dynamique</a></li>
+		 <li><a href=http://intranet.wt17.ephec-ti.be>Intranet</a></li>
+		</ul>
+	</nav>
+    <h1>Site Dynamique [b2b]</h1>
+	<section>
+		<fieldset>
+			<legend> Répartition du travail </legend>
+			<article>
+				<h4>Scuflaire Thomas 2TL2</h4>
+				<hr>
+				<p> Responsable de la partie web. Mise en place du serveur apache 7.0, la configuration
+					se trouve sur Github qui est relié à Dockerhub ce qui permet le build automatique à 
+					chaques modifications.</p>
+			</article>
+			<article>
+				<h4>Marquebreucq Emmanuel 2TL2</h4>
+				<hr>
+				<p> Responsable de la partie DNS. </p>
+			</article>
+			<article>
+				<h4>Brancart Clément 2TL2</h4>
+				<hr>
+				<p> Responsable de la partie Base de Donnée. Utilisation des bdd Mysql.</p>
+			</article>
+		</fieldset>
+	</section>
+	<hr>
+    <p> Un tableau prouvant la connexion à la db sera affiché ici quand elle sera disponible<br><br> Adresse du site : <b>b2b.wt17.ephec-ti.be</b> </p>
+  </body>
+</html>
+
+
